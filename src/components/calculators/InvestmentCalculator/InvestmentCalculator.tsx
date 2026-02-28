65bd0b56a33edc35f883f5dadb814bef9b121684
Jackson Wozniak <jjw30@comcast.net>
2026-02-27 20:15:41 -0500

    basic scaffolding

---
+++ src/components/calculators/InvestmentCalculator/InvestmentCalculator.tsx
@@ -0,0 +1,15 @@
+import Box from "@mui/material/Box";
+
+const InvestmentCalculator: React.FC<{
+
+}> = ({}) => {
+    document.title = "Investment Calculator";
+
+    return (
+        <Box>
+            
+        </Box>
+    );
+};
+
+export default InvestmentCalculator;
